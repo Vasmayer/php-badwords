76eed15adb6858c8a7feaeeafa3378f8075445cf
Vasmayer <lealimarco90@gmail.com>
2022-02-24 14:54:29 +0100

    point 1/2 completed

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito 
 con tre asterischi (***) tutte le occorrenze della parola da censurare. */
+
+$text  = 'Lorem ipsum parola dolor sit amet parola consectetur adipisicing parola elit.'; 
+$length = strlen($text);
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +19,6 @@ con tre asterischi (***) tutte le occorrenze della parola da censurare. */
   <title>php-badwords</title>
 </head>
 <body>
-
+<h3> La stringa è "<?php echo $text ?>" ed è lunga <?php echo $length ?> caratteri</h3>
 </body>
 </html>
